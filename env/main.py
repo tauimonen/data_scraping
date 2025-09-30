6f6226a3b69964577a6ac3fbab747fec064b4ac0
tauimonen <tauimonen@gmail.com>
2025-09-30 11:39:47 +0300

    Print h5 headers

--- env/main.py
+++ env/main.py
@@ -1,11 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://helsinginsuunnistajat.fi/"
+with open("index.html") as file:
+    content = file.read()
+
+soup = BeautifulSoup(content, "lxml")
+h5 = soup.find_all("h5")
+print(h5)
+for item in h5:
+    print(item)
+
+list(map(print, h5))
+
+
+""" url = "https://helsinginsuunnistajat.fi/"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 print(soup.prettify())
 
 
 for i in soup.find_all('p'): 
-    print(i.text.replace("\n", " ").strip())
+    print(i.text.replace("\n", " ").strip()) """
+
+## ctrl-ö = toggle terminal
+# ctrl-k-c = kommentoi
+# ctrl-k-u = pois
